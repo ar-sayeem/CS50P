abc260cce142219e238d21a0cdf4405f3f1ed874
Adnan Rahman Sayeem <adnan.rahman.sayeem@mail.com>
2024-01-02 15:16:09 +0600

    update

--- Object-Oriented Programming/student.py
+++ Object-Oriented Programming/student.py
@@ -1,1 +1,18 @@
-print("student")
+# tuple - IMMUTABLE - can't be change
+# tuple vs list
+
+def main():
+    student = get_student()
+    if student[0] == "Padma":
+        student[1] = "Ravenclaw"
+    print(f"{student[0]} from {student[1]}")
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return [name, house]
+    # List - [] Mutable
+    # Tuple - () Immutable - can't change inside of tuple (such as, using if)
+
+if __name__ == "__main__":
+    main()
