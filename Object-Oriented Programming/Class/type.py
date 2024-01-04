b06520651cc5f09bb4c9ec2f0bb9698a3145ee45
Adnan Rahman Sayeem <adnan.rahman.sayeem@mail.com>
2024-01-04 08:13:02 +0600

    update

--- Object-Oriented Programming/Class/type.py
+++ Object-Oriented Programming/Class/type.py
@@ -1,2 +1,8 @@
-# various data types
-int 
+### various data types ###
+
+print(type(50))         # int
+print(type("Hello"))    # str
+print(type([]))         # list
+print(type(list()))     # list
+print(type({}))         # dict
+print(type(dict()))     # dict
