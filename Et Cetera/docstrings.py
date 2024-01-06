c858e0d772e702d10fd85306c594fb5a966301a4
Adnan Rahman Sayeem <adnan.rahman.sayeem@mail.com>
2024-01-06 18:59:21 +0600

    update

--- Et Cetera/docstrings.py
+++ Et Cetera/docstrings.py
@@ -1,2 +1,20 @@
 # Python documentation strings (or docstrings) provide a convenient way
 # of associating documentation with Python modules, functions, classes, and methods.
+# """ ... """ or ''' ... '''
+
+
+def meow(n: int) -> str:
+    """
+    Meow n times.
+    
+    : param n: Number of times to meow
+    : type n: int
+    : raise TypeError: If is not an int
+    : return: A string of n meows, one per line
+    """
+    return "meow\n" * n
+
+
+number: int = int(input("Number: "))
+meows: str = meow(number)
+print(meows, end="")
