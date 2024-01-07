0c9baedc69355a84c5d840f91de34e1322a603df
Adnan Rahman Sayeem <adnan.rahman.sayeem@mail.com>
2024-01-07 10:38:38 +0600

    update

--- Et Cetera/argparse_meows.py
+++ Et Cetera/argparse_meows.py
@@ -2,3 +2,13 @@
 # The ' argparse' module in Python helps create a program in a command-line-environment
 # in a way that appears not only easy to code but also improves interaction.
 
+import argparse
+
+parser = argparse.ArgumentParser(description="Meow like a cat")
+# Add an argument with the name "--name" and its description as "The person'
+
+parser.add_argument("--n", default=1, help="number of times to meow", type=int)
+args = parser.parse_args()
+
+for _ in range(int(args.n)):
+    print('meow')
